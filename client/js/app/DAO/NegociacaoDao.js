b049c89df540c99ceb760bbfa39880ed8a593952
Miguel Pereira Ramos <79910097+Pererote@users.noreply.github.com>
2021-04-17 16:33:02 -0300

    IDB "API"

---
+++ client/js/app/DAO/NegociacaoDao.js
@@ -0,0 +1,19 @@
+class NegociacaoDao {
+	constructor(connection) {
+		this._connection = connection;
+		this._store = "negociacoes";
+	}
+
+	adiciona(negociacao) {
+		return new Promise((resolve, reject) => {
+			let request = this._connection
+				.transaction([this._store], "readwrite")
+				.objectStore(this._store)
+				.add(negociacao);
+
+			request.onsuccess = (e) => resolve();
+			request.onerror = (e) =>
+				reject("Não foi possível adicionar a negociação.");
+		});
+	}
+}
